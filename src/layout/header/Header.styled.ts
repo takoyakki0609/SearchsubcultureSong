c37f1483294d498e08a3ee438f7a06b5ea10c470
takoyakki0609 <kjy6478@naver.com>
2024-04-20 00:31:34 +0900

    style: navbar, ani,utaite,vocaloid components 삭제

--- src/layout/header/Header.styled.ts
+++ src/layout/header/Header.styled.ts
@@ -9,23 +9,6 @@ export const TItle = styled.h1`
   font-size: 3rem;
 `;
 
-/* navBar style */
-export const DataWrapper = styled.ul`
-  font-size: 2rem;
-
-  margin-top: 10rem;
-
-  display: flex;
-  justify-content: center;
-  gap: 2rem;
-`;
-export const DataTitle = styled.li`
-  &:hover {
-    cursor: pointer;
-    color: #fca311;
-    transition: 0.3s;
-  }
-`;
 /* searchInput style*/
 export const SearchWrapper = styled.form`
   margin-top: 5rem;
--- src/layout/header/Header.tsx
+++ src/layout/header/Header.tsx
@@ -1,20 +1,16 @@
-import { Link } from "react-router-dom";
 import * as St from "./Header.styled";
 import SearchInput from "./SearchInput";
-import Category from "./category/Category";
 import { useRecoilValue } from "recoil";
-import { clickState } from "../../state/Atom";
-import NavBar from "./navbar/NavBar";
-
+import { typeState } from "../../state/Atom";
 const Header = () => {
-  const selectness = useRecoilValue(clickState);
+  const selectness = useRecoilValue(typeState);
   return (
     <St.Container>
       <St.TitleWrapper>
         <St.TItle>노래방 번호 검색기</St.TItle>
+        <>선택된 노래방 기기는 {selectness} 입니다.</>
       </St.TitleWrapper>
-      {selectness ? <NavBar /> : <Category />}
-      {selectness ? <SearchInput /> : <></>}
+      <SearchInput />
     </St.Container>
   );
 };
