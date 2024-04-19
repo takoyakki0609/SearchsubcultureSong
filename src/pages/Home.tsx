fb30156b44358d811fbdb2f0196cebc8482616f8
takoyakki0609 <kjy6478@naver.com>
2024-04-20 03:50:44 +0900

    style : animation json 수정

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,20 @@
+import { useRecoilValue } from "recoil";
+import { searchDataState } from "../state/Atom";
+import SearchResult from "./searchResult/SearchResult";
+
 const Home = () => {
-  return <></>;
+  const songData = useRecoilValue(searchDataState);
+  return (
+    <>
+      {songData ? (
+        <>
+          <SearchResult />
+        </>
+      ) : (
+        <>검색결과가 없습니다.</>
+      )}
+    </>
+  );
 };
 
 export default Home;
